221d21bd89c2dbc15ced60c39751f28f04c54394
andresmorales8 <moralesandres62@gmail.com>
2019-11-14 20:47:42 -0500

    Agrega Unity juego-cuatro-en-linea
    Se agrega unity, junto con los scripts de la grid de las esferas que cambian de color azul al verde pasar el mouse por encima,

---
+++ juego-cuatro-en-linea/Assets/Grid2D.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid2D : MonoBehaviour
+{
+    public int width;
+    public int height;
+    public GameObject puzzlePiece;
+    private GameObject[,] grid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new GameObject[width, height];
+        for (int x = 0; x<width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                GameObject go = GameObject.Instantiate(puzzlePiece) as GameObject;
+                Vector3 position = new Vector3(x, y, 0);
+                go.transform.position = position;
+                grid[x,y]= go;
+                
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        UpdatePickedPiece(mPosition);
+    }
+
+    void UpdatePickedPiece(Vector3 position)
+    {
+        Debug.Log(position.x + " - " + position.y);
+        int x = (int)(position.x + 0.5f);
+        int y = (int)(position.y + 0.5f);
+        for(int _x = 0; _x < width; _x++)
+        {
+            for(int _y = 0; _y < height; _y++)
+            {
+                GameObject go = grid[_x, _y];
+                go.GetComponent<Renderer>().material.SetColor("_Color", Color.blue);
+
+            }
+
+        }
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            GameObject go = grid[x, y];
+            Debug.Log(x+" - "+y);
+            go.GetComponent<Renderer>().material.SetColor("_Color", Color.green);
+        }
+    }
+}
